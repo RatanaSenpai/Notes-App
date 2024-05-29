a6401ac17a000e2d2e20aa82eaf25a2ab15275ef
Ratana <createbysenpai@gmail.com>
2024-05-29 14:44:38 +0200

    🏗️ Add EJS templating setup with main layout and index view

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
-const port = 5000 || process.env.port;
+const port = 4000 || process.env.port;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
