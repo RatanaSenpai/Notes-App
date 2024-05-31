80b0c37a5da76ca5b4d82bc0fc8a272c3d762a2f
Ratana <createbysenpai@gmail.com>
2024-05-31 15:43:48 +0200

    ✨ Handle 404 error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const expressLayouts = require("express-ejs-layouts");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const port = 4000 || process.env.port;
@@ -10,15 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static Files
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //Templating Engine
 app.use(expressLayouts);
-app.set("layout", "./layouts/main");
-app.set("view engine", "ejs");
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 // Routes
-app.use("/", require("./server/routes/index"));
+app.use('/', require('./server/routes/index'));
+
+// Handle 404
+app.get('*', function (req, res) {
+  res.status(404).render('404');
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
